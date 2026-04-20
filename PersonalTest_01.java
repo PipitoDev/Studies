eeff626e738983fe5261cc9ccaeb875092379379
PipitoDev <joaovitorsguizzato@gmail.com>
2026-04-19 23:29:21 -0300

    Rename class from _PersonalTest1 to PersonalTest_01

--- PersonalTest_01.java
+++ PersonalTest_01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class _PersonalTest1 {
+public class PersonalTest_01 {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
     int num, num1;
